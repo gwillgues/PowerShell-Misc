fe052e07a3b402e9d00752e3db3c11076eb9bc33
gwillgues <96144967+gwillgues@users.noreply.github.com>
2022-09-02 10:15:29 -0500

    Create function with -file parameter, change get-content to use -raw
    Improves support for scripts without semicolons at the end of the line.

--- encode_script.ps1
+++ encode_script.ps1
@@ -1,5 +1,15 @@
 #Base64 encode a powershell script for usability with "Powershell.exe -encodedcommand" 
+Function encode-script {
+	param (
+          [Parameter(Mandatory=$true)]
+          [ValidateNotNullOrEmpty()]
+          [string]
+          $file
+	  )
 
-$command = get-content ./Documents/script.ps1;
+
+$command = get-content -raw $file;
 $a = [Text.Encoding]::Unicode.GetBytes($command);
 [Convert]::ToBase64String($a);
+
+}
