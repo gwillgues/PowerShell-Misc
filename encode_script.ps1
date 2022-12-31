d69150a09cc3ab7755800eb44c38b4e5f5815cdf
gwillgues <96144967+gwillgues@users.noreply.github.com>
2022-12-30 19:14:09 -0600

    Change encode_script.ps1 from function to script for command line parameter usage

--- encode_script.ps1
+++ encode_script.ps1
@@ -1,15 +1,13 @@
-#Base64 encode a powershell script for usability with "Powershell.exe -encodedcommand" 
-Function encode-script {
-	param (
+#Base64 encode a powershell script for usability with "Powershell.exe -encodedcommand"
+
+param (
           [Parameter(Mandatory=$true)]
           [ValidateNotNullOrEmpty()]
           [string]
           $file
-	  )
+          )
 
 
 $command = get-content -raw $file;
 $a = [Text.Encoding]::Unicode.GetBytes($command);
 [Convert]::ToBase64String($a);
-
-}
